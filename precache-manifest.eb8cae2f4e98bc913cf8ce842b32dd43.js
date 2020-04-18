b3d9c3f4645fc385ebeefb4087f021433b367bfc
izzudinhafiz <izzudin.hafiz@gmail.com>
2020-04-18 23:30:54 +0800

    Updates

---
+++ precache-manifest.eb8cae2f4e98bc913cf8ce842b32dd43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d729ad3cc3b620631cf811c07839b2",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "41b6be28af8c0b97c61c",
+    "url": "/covid19/static/css/main.832c73d7.chunk.css"
+  },
+  {
+    "revision": "658dc18d65b69293f5b1",
+    "url": "/covid19/static/js/2.eedb307f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.eedb307f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b6be28af8c0b97c61c",
+    "url": "/covid19/static/js/main.f0260818.chunk.js"
+  },
+  {
+    "revision": "6671708e50e0d289ff53",
+    "url": "/covid19/static/js/runtime-main.79b38e59.js"
+  }
+]);
